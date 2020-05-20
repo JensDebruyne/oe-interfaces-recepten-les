573b5fd774fc4ed8c0f061db783c5cbf0b2d3f8e
Stef Spermalie <stef.vercaemer@spermalie.be>
2020-05-20 18:06:06 +0200

    Add project files.

---
+++ Recepten.Lib/Services/MockDataArtikelen.cs
@@ -0,0 +1,77 @@
+﻿using Recepten.Lib.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recepten.Lib.Services
+{
+    public class MockDataArtikelen
+    {
+        public List<Artikel> Artikelen { get; set; }
+
+        public MockDataArtikelen()
+        {
+            LaadArtikelen();
+        }
+
+        public void SlaOp(Artikel opTeSlaan)
+        {
+            if (opTeSlaan == null) throw new Exception("Geef een geldig artikel door om op te slaan");
+            else if (!BehoortObjectTotLijst(opTeSlaan)) Artikelen.Add(opTeSlaan);
+            else
+            {
+                int indexObject = GeefIndexInLijst(opTeSlaan);
+                Artikelen[indexObject] = opTeSlaan;
+            }
+        }
+
+        public void Verwijder(Artikel teVerwijderen)
+        {
+            if (teVerwijderen != null && BehoortObjectTotLijst(teVerwijderen))
+            {
+                Artikelen.Remove(teVerwijderen);
+            }
+            else throw new Exception("Geef een geldig artikel door om te verwijderen");
+        }
+
+        int GeefIndexInLijst(Artikel teChecken)
+        {
+            int indexTeCheckenInstance = -1;
+            for (int i = 0; i < Artikelen.Count; i++)
+            {
+                if (Artikelen[i].Id == teChecken.Id)
+                {
+                    indexTeCheckenInstance = i;
+                    break;
+                }
+            }
+            return indexTeCheckenInstance;
+        }
+
+        bool BehoortObjectTotLijst(Artikel teChecken)
+        {
+            bool gevonden = false;
+            foreach (Artikel instance in Artikelen)
+            {
+                if (instance.Id == teChecken.Id)
+                {
+                    gevonden = true;
+                    break;
+                }
+            }
+            return gevonden;
+        }
+
+        public void LaadArtikelen()
+        {
+            Artikelen = new List<Artikel>
+            {
+                new Artikel("ei", "Stuk", 0.25M),
+                new Artikel("ui", "Kg", 0.65M),
+                new Artikel("kreeft", "Stuk", 24.50M)
+            };
+        }
+    }
+}
---
+++ Recepten.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Recepten.Lib.Entities;
+using Recepten.Lib.Services;
+
+
+
+namespace Recepten.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Artikel huidigArtikel;
+        EmptyDataArtikelen artikelService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            artikelService = new EmptyDataArtikelen();
+        }
+
+        private void MaakGuiLeeg()
+        {
+            lstArtikelen.SelectedItem = null;
+            ClearPanel(grdArtikel);
+            cmbEenheid.SelectedIndex = 0;
+            txtPrijs.Text = "0";
+            txtArtikelnaam.Focus();
+            tbkFeedback.Visibility = Visibility.Hidden;
+        }
+
+        Artikel GeefIngegevenArtikel()
+        {
+            Artikel ingegevenArtikel = null;
+            string naam = txtArtikelnaam.Text;
+            string eenheid = cmbEenheid.SelectedItem.ToString();
+            try
+            {
+                decimal prijs = decimal.Parse(txtPrijs.Text);
+                try
+                {
+                    int id = (huidigArtikel != null) ? huidigArtikel.Id : 0;
+                    ingegevenArtikel = new Artikel(naam, eenheid, prijs, id);
+                }
+                catch (Exception ex)
+                {
+                    ToonMelding(ex.Message);
+                }
+
+            }
+            catch (Exception)
+            {
+                ToonMelding("De prijs is geen geldig getal");
+            }
+            return ingegevenArtikel;
+        }
+
+        void ToonArtikelen()
+        {
+            lstArtikelen.ItemsSource = artikelService.Artikelen;
+            lstArtikelen.Items.Refresh();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LaadBeginSituatie();
+        }
+
+        void LaadBeginSituatie()
+        {
+            ToonArtikelen();
+            cmbEenheid.ItemsSource = Artikel.Verpakkingen;
+            lstArtikelen.SelectedIndex = 0;
+            tbkFeedback.Visibility = Visibility.Hidden;
+            txtPrijs.TextChanged += DecimalTextBox_TextChanged;
+        }
+
+        private void lstArtikelen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            huidigArtikel = (Artikel)lstArtikelen.SelectedItem;
+            if (huidigArtikel != null)
+            {
+                lblId.Content = huidigArtikel.Id;
+                txtArtikelnaam.Text = huidigArtikel.Naam;
+                txtPrijs.Text = huidigArtikel.Prijs.ToString();
+                cmbEenheid.SelectedItem = huidigArtikel.Eenheid;
+            }
+            else ClearPanel(grdArtikel);
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            MaakGuiLeeg();
+        }
+
+        private void btnVerwijder_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string artikelNaam = (huidigArtikel != null) ? huidigArtikel.Naam : "";
+                artikelService.Verwijder(huidigArtikel);
+                ToonArtikelen();
+                MaakGuiLeeg();
+                ToonMelding($"{artikelNaam} is verwijderd", true);
+            }
+            catch (Exception ex)
+            {
+                ToonMelding(ex.Message);
+            }
+            
+        }
+
+        private void btnSlaOp_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                huidigArtikel = GeefIngegevenArtikel();
+                if(huidigArtikel != null)
+                {
+                    string artikelNaam = (huidigArtikel != null) ? huidigArtikel.Naam : "";
+                    artikelService.SlaOp(huidigArtikel);
+                    ToonArtikelen();
+                    MaakGuiLeeg();
+                    ToonMelding($"{artikelNaam} is opgeslagen", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                ToonMelding(ex.Message);
+            }
+        }
+
+    }
+}
