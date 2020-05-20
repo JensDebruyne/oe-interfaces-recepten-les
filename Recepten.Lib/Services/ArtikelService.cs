5a052b7827d3881a19719470572d5d04454730cb
Stef Spermalie <stef.vercaemer@spermalie.be>
2020-05-20 19:54:44 +0200

    Add ArtikelService.

---
+++ Recepten.Lib/Services/ArtikelService.cs
@@ -0,0 +1,33 @@
+﻿using Recepten.Lib.Entities;
+using Recepten.Lib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recepten.Lib.Services
+{
+    public class ArtikelService
+    {
+        private IArtikelData gekozenDataSource;
+        
+        public List<Artikel> Artikelen { get; set; }
+
+        public ArtikelService(IArtikelData dataSource)
+        {
+            Artikelen = dataSource.Artikelen;
+            gekozenDataSource = dataSource;
+        }
+
+        public void Verwijder(Artikel teVerwijderen)
+        {
+            gekozenDataSource.Verwijder(teVerwijderen);
+        }
+
+        public void SlaOp(Artikel opTeSlaan)
+        {
+            gekozenDataSource.SlaOp(opTeSlaan);
+        }
+    }
+}
